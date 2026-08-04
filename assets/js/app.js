69226ad269b15424e677ee9431dd089fa83bc2fc
imammehedihasan07-cyber <imammehedihasan07@gmail.com>
2026-08-04 17:04:52 +0600

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -64,3 +64,9 @@ function shareCurrentTool() {
         });
     }
 }
+// Register Service Worker for PWA
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js').catch(() => {});
+  });
+}
